a3bb339c5fa1beeed4719f1e09f6b98bfd0dd63d
Sudipta Sundar Pal <Sudiptasundarpal@gmail.com>
2025-04-21 19:42:31 +0530

    Made a small change to trigger Jenkins build

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Microservice!");
+        System.out.println("Hello from Microservice! this is sudipta");
     }
 }
